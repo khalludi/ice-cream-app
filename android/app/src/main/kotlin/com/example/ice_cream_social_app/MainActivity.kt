9e7cc3998fe38c93b6c0ee493f941d276e53f9ad
Payal <payalahuja98@gmail.com>
2021-03-20 21:36:26 -0500

    updates for home, product pages

--- android/app/src/main/kotlin/com/example/ice_cream_social_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ice_cream_social_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.ice_cream_social
+package com.example.ice_cream_social_app
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ lib/search.dart
@@ -0,0 +1,3 @@
+/**
+ * TO DO: Autocomplete feature??
+ */
