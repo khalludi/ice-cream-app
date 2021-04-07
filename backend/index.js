8bb85a0362cf7c655db31da5feffc3fca23b7266
hwishn <45208429+hwishn@users.noreply.github.com>
2021-04-06 21:55:26 -0500

    all of my backend functions
    terraform apply works now!

---
+++ backend/index.js
@@ -0,0 +1,56 @@
+/**
+ *  Return Ingredients
+ */
+
+const mysql = require('promise-mysql');
+
+const createTcpPool = async config => {
+  // Extract host and port from socket address
+  // const dbSocketAddr = process.env.DB_HOST.split(':');
+
+  // Establish a connection to the database
+  return await mysql.createPool({
+    user: "testuser", // e.g. 'my-db-user'
+    password: "testuser", // e.g. 'my-db-password'
+    database: "icecreamdb", // e.g. 'my-database'
+    host: '10.91.0.3', // e.g. '127.0.0.1'
+    port: '3306', // e.g. '3306'
+    // ... Specify additional properties here.
+    ...config,
+  });
+};
+
+const createPool = async () => {
+  const config = {
+    // [START cloud_sql_mysql_mysql_limit]
+    // 'connectionLimit' is the maximum number of connections the pool is allowed
+    // to keep at once.
+    connectionLimit: 5,
+    // [END cloud_sql_mysql_mysql_limit]
+
+    // [START cloud_sql_mysql_mysql_timeout]
+    // 'connectTimeout' is the maximum number of milliseconds before a timeout
+    // occurs during the initial connection to the database.
+    connectTimeout: 10000, // 10 seconds
+    // 'acquireTimeout' is the maximum number of milliseconds to wait when
+    // checking out a connection from the pool before a timeout error occurs.
+    acquireTimeout: 10000, // 10 seconds
+    // 'waitForConnections' determines the pool's action when no connections are
+    // free. If true, the request will queued and a connection will be presented
+    // when ready. If false, the pool will call back with an error.
+    waitForConnections: true, // Default: true
+    // 'queueLimit' is the maximum number of requests for connections the pool
+    // will queue at once before returning an error. If 0, there is no limit.
+    queueLimit: 0, // Default: 0
+    // [END cloud_sql_mysql_mysql_timeout]
+  };
+  return await createTcpPool(config);
+};
+
+let pool;
+
+exports.ingredientsSET = async (req, res) => {
+  pool = await createPool();
+  const talentiQuery = pool.query("UPDATE Ingredients SET ingredient_id = '" +  req.body.ingredient_id + "', name = '"+ req.body.name + "' + WHERE ingredient_id = '" + req.body.oldIngredient_id);
+  res.send("Update Complete!");
+}
--- backend/main.tf
+++ backend/main.tf
@@ -32,9 +32,9 @@ resource "google_service_networking_connection" "private_vpc_connection" {
   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
 }
 
-resource "google_vpc_access_connector" "connector" {
+resource "google_vpc_access_connector" "my_connector" {
   provider      = google-beta
-  name          = "vpc-con"
+  name          = "hannah-connector"
   ip_cidr_range = "10.8.0.0/28"
   network       = google_compute_network.private_network.name
 }
@@ -44,7 +44,7 @@ resource "google_vpc_access_connector" "connector" {
 # MAKE DATABASE
 #
 ######
-resource "google_sql_database_instance" "instance" {
+/*resource "google_sql_database_instance" "instance" {
   provider = google-beta
 
   name   = "icecreamdb-tf"
@@ -66,7 +66,7 @@ resource "google_sql_database_instance" "instance" {
     # prevent_destroy = true
     ignore_changes = all
   }
-}
+}*/
 
 # resource "google_sql_user" "users" {
 #   provider = google-beta
@@ -95,6 +95,82 @@ resource "google_storage_bucket_object" "archive" {
   source = "./reviews_get/get_reviews.zip"
 }
 
+resource "google_storage_bucket_object" "archive2" {
+  provider = google-beta
+  name   = "get_reviews.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./get_reviews.zip"
+}
+
+resource "google_storage_bucket_object" "archive3" {
+  provider = google-beta
+  name   = "add_reviews.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./add_reviews.zip"
+}
+
+resource "google_storage_bucket_object" "archive4" {
+  provider = google-beta
+  name   = "delete_ingredients.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./delete_ingredients.zip"
+}
+
+resource "google_storage_bucket_object" "archive5" {
+  provider = google-beta
+  name   = "delete_reviews.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./delete_reviews.zip"
+}
+
+resource "google_storage_bucket_object" "archive6" {
+  provider = google-beta
+  name   = "get_allingredients.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./get_allingredients.zip"
+}
+
+resource "google_storage_bucket_object" "archive7" {
+  provider = google-beta
+  name   = "get_allreviews.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./get_allreviews.zip"
+}
+
+resource "google_storage_bucket_object" "archive8" {
+  provider = google-beta
+  name   = "get_ingredients.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./get_ingredients.zip"
+}
+
+resource "google_storage_bucket_object" "archive9" {
+  provider = google-beta
+  name   = "add_ingredients.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./add_ingredients.zip"
+}
+
+resource "google_storage_bucket_object" "archive10" {
+  provider = google-beta
+  name   = "get_reviewstext.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./get_reviewstext.zip"
+}
+
+resource "google_storage_bucket_object" "archive11" {
+  provider = google-beta
+  name   = "update_ingredients.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./update_ingredients.zip"
+}
+
+resource "google_storage_bucket_object" "archive12" {
+  provider = google-beta
+  name   = "update_reviews.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./update_reviews.zip"
+}
 ######
 #
 # MAKE CLOUD FUNCTION
@@ -106,7 +182,7 @@ resource "google_cloudfunctions_function" "function" {
   description = "My function"
   runtime     = "nodejs10"
 
-  vpc_connector         = google_vpc_access_connector.connector.id
+  vpc_connector         = google_vpc_access_connector.my_connector.id
   available_memory_mb   = 128
   source_archive_bucket = google_storage_bucket.bucket.name
   source_archive_object = google_storage_bucket_object.archive.name
@@ -117,6 +193,261 @@ resource "google_cloudfunctions_function" "function" {
 resource "google_cloudfunctions_function_iam_binding" "binding" {
   provider = google-beta
   cloud_function = google_cloudfunctions_function.function.name
+  role = "roles/cloudfunctions.invoker"
+  members = [
+    "allUsers",
+  ]
+}
+
+resource "google_cloudfunctions_function" "function2" {
+  provider = google-beta
+  name        = "function-getReviews"
+  description = "My function"
+  runtime     = "nodejs10"
+
+  vpc_connector         = google_vpc_access_connector.my_connector.id
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive2.name
+  trigger_http          = true
+  entry_point           = "reviewsGET"
+}
+
+resource "google_cloudfunctions_function_iam_binding" "binding2" {
+  provider = google-beta
+  cloud_function = google_cloudfunctions_function.function2.name
+  role = "roles/cloudfunctions.invoker"
+  members = [
+    "allUsers",
+  ]
+}
+
+resource "google_cloudfunctions_function" "function3" {
+  provider = google-beta
+  name        = "function-addReviews"
+  description = "My function"
+  runtime     = "nodejs10"
+
+  vpc_connector         = google_vpc_access_connector.my_connector.id
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive3.name
+  trigger_http          = true
+  entry_point           = "ingredientsADD"
+}
+
+resource "google_cloudfunctions_function_iam_binding" "binding3" {
+  provider = google-beta
+  cloud_function = google_cloudfunctions_function.function3.name
+  role = "roles/cloudfunctions.invoker"
+  members = [
+    "allUsers",
+  ]
+}
+
+resource "google_cloudfunctions_function" "function4" {
+  provider = google-beta
+  name        = "function-deleteIngredients"
+  description = "My function"
+  runtime     = "nodejs10"
+
+  vpc_connector         = google_vpc_access_connector.my_connector.id
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive4.name
+  trigger_http          = true
+  entry_point           = "ingredientsDELETE"
+}
+
+resource "google_cloudfunctions_function_iam_binding" "binding4" {
+  provider = google-beta
+  cloud_function = google_cloudfunctions_function.function4.name
+  role = "roles/cloudfunctions.invoker"
+  members = [
+    "allUsers",
+  ]
+}
+
+resource "google_cloudfunctions_function" "function5" {
+  provider = google-beta
+  name        = "function-deleteReviews"
+  description = "My function"
+  runtime     = "nodejs10"
+
+  vpc_connector         = google_vpc_access_connector.my_connector.id
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive5.name
+  trigger_http          = true
+  entry_point           = "reviewsDELETE"
+}
+
+resource "google_cloudfunctions_function_iam_binding" "binding5" {
+  provider = google-beta
+  cloud_function = google_cloudfunctions_function.function5.name
+  role = "roles/cloudfunctions.invoker"
+  members = [
+    "allUsers",
+  ]
+}
+
+resource "google_cloudfunctions_function" "function6" {
+  provider = google-beta
+  name        = "function-getAllIngredients"
+  description = "My function"
+  runtime     = "nodejs10"
+
+  vpc_connector         = google_vpc_access_connector.my_connector.id
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive6.name
+  trigger_http          = true
+  entry_point           = "allIngredientsGET"
+}
+
+resource "google_cloudfunctions_function_iam_binding" "binding6" {
+  provider = google-beta
+  cloud_function = google_cloudfunctions_function.function6.name
+  role = "roles/cloudfunctions.invoker"
+  members = [
+    "allUsers",
+  ]
+}
+
+resource "google_cloudfunctions_function" "function7" {
+  provider = google-beta
+  name        = "function-getAllReviews"
+  description = "My function"
+  runtime     = "nodejs10"
+
+  vpc_connector         = google_vpc_access_connector.my_connector.id
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive7.name
+  trigger_http          = true
+  entry_point           = "allReviewsGET"
+}
+
+resource "google_cloudfunctions_function_iam_binding" "binding7" {
+  provider = google-beta
+  cloud_function = google_cloudfunctions_function.function7.name
+  role = "roles/cloudfunctions.invoker"
+  members = [
+    "allUsers",
+  ]
+}
+
+resource "google_cloudfunctions_function" "function8" {
+  provider = google-beta
+  name        = "function-getIngredients"
+  description = "My function"
+  runtime     = "nodejs10"
+
+  vpc_connector         = google_vpc_access_connector.my_connector.id
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive8.name
+  trigger_http          = true
+  entry_point           = "ingredientsGET"
+}
+
+
+
+resource "google_cloudfunctions_function_iam_binding" "binding8" {
+  provider = google-beta
+  cloud_function = google_cloudfunctions_function.function8.name
+  role = "roles/cloudfunctions.invoker"
+  members = [
+    "allUsers",
+  ]
+}
+
+resource "google_cloudfunctions_function" "function9" {
+  provider = google-beta
+  name        = "function-addIngredients"
+  description = "My function"
+  runtime     = "nodejs10"
+
+  vpc_connector         = google_vpc_access_connector.my_connector.id
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive9.name
+  trigger_http          = true
+  entry_point           = "ingredientsADD"
+}
+
+resource "google_cloudfunctions_function_iam_binding" "binding9" {
+  provider = google-beta
+  cloud_function = google_cloudfunctions_function.function9.name
+  role = "roles/cloudfunctions.invoker"
+  members = [
+    "allUsers",
+  ]
+}
+
+resource "google_cloudfunctions_function" "function10" {
+  provider = google-beta
+  name        = "function-getReviewsText"
+  description = "My function"
+  runtime     = "nodejs10"
+
+  vpc_connector         = google_vpc_access_connector.my_connector.id
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive10.name
+  trigger_http          = true
+  entry_point           = "reviewsTextGET"
+}
+
+resource "google_cloudfunctions_function_iam_binding" "binding10" {
+  provider = google-beta
+  cloud_function = google_cloudfunctions_function.function10.name
+  role = "roles/cloudfunctions.invoker"
+  members = [
+    "allUsers",
+  ]
+}
+
+resource "google_cloudfunctions_function" "function11" {
+  provider = google-beta
+  name        = "function-updateIngredients"
+  description = "My function"
+  runtime     = "nodejs10"
+
+  vpc_connector         = google_vpc_access_connector.my_connector.id
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive11.name
+  trigger_http          = true
+  entry_point           = "ingredientsSET"
+}
+
+resource "google_cloudfunctions_function_iam_binding" "binding11" {
+  provider = google-beta
+  cloud_function = google_cloudfunctions_function.function11.name
+  role = "roles/cloudfunctions.invoker"
+  members = [
+    "allUsers",
+  ]
+}
+
+resource "google_cloudfunctions_function" "function12" {
+  provider = google-beta
+  name        = "function-updateReviews"
+  description = "My function"
+  runtime     = "nodejs10"
+
+  vpc_connector         = google_vpc_access_connector.my_connector.id
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive12.name
+  trigger_http          = true
+  entry_point           = "reviewsUPDATE"
+}
+
+resource "google_cloudfunctions_function_iam_binding" "binding12" {
+  provider = google-beta
+  cloud_function = google_cloudfunctions_function.function12.name
   role = "roles/cloudfunctions.invoker"
   members = [
     "allUsers",
