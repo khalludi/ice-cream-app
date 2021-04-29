cd387d56480feb376a1d837ad869f485dcc352ea
Payal <payalahuja98@gmail.com>
2021-04-29 16:05:00 -0500

    latest frontend product cards

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,10 @@ import 'package:ice_cream_social/login/login_screen.dart';
 import 'package:ice_cream_social/login/profile.dart';
 import 'package:ice_cream_social/login/authentication.dart';
 import 'HomePage/filter.dart';
-import 'HomePage/placeholder_widget.dart';
 import 'package:ice_cream_social/backend_data.dart';
+import 'HomePage/Products.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 
 void main() {
@@ -31,6 +33,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Future<List<Products>> futureProducts;
+  List<Products> products;
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  BackendData providerBackendData;
+  String url;
+  String username;
+  String password;
+
+  @override
+  void initState() {
+    providerBackendData = Provider.of<BackendData>(
+      context,
+      listen: false,
+    );
+    url = providerBackendData.url;
+    username = providerBackendData.username;
+    password = providerBackendData.password;
+    futureProducts = fetchProducts();
+    super.initState();
+  }
   int _selectedIndex = 0;
   int loginChanged = 0;
   static const TextStyle optionStyle =
@@ -65,6 +87,29 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  Future<List<Products>> fetchProducts() async {
+    String basicAuth =
+        'Basic ' + base64Encode(utf8.encode('$username:$password'));
+    final response = await http.get(Uri.https(url, "get-product-all"), headers: {
+      "Accept": "application/json",
+      'authorization': basicAuth,
+    });
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body);
+      print(data);
+      // var rest = data as List;
+      List<Products> products =
+      (data).map((i) => Products.fromJson(i)).toList();
+      //print(product);
+      return products;
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      print("Fail");
+    }
+    return null;
+  }
+
   Widget build(BuildContext context) {
     Authentication auth = new Authentication();
     _widgetOptions = [];
@@ -79,7 +124,15 @@ class _HomePageState extends State<HomePage> {
         home: Scaffold(
           // appBar: _buildBar(context),
           body: Builder(
-            builder: (context) => chooseWidget(context)
+            //builder: (context) => chooseWidget(context),
+            builder: (context) => buildFuture(context)
+          ),
+          floatingActionButton: Builder(
+            builder: (context) => FloatingActionButton(
+              //onPressed: launchAddDialog,
+              child: Icon(Icons.add),
+              backgroundColor: Colors.purple,
+            ),
           ),
           /**Bottom navigation drawer.**/
           bottomNavigationBar: BottomNavigationBar(
@@ -101,6 +154,34 @@ class _HomePageState extends State<HomePage> {
       );
   }
 
+  Widget buildFuture(BuildContext context) {
+    return FutureBuilder<List<Products>>(
+      future: futureProducts,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          products = snapshot.data;
+          print(products);
+          return buildList(context);
+        } else if (snapshot.hasError) {
+          print(snapshot.error);
+          return Scaffold(
+            key: scaffoldKey,
+            appBar: _buildBar(context),
+            body: Center(
+              child: Text("Error getting products data"),
+            ),
+          );
+        }
+        // By default, show a loading spinner.
+        return Scaffold(
+          key: scaffoldKey,
+          appBar: _buildBar(context),
+          //body: buildSearch(context),
+        );
+      },
+    );
+  }
+
   Widget _buildBar(BuildContext context) {
     return new AppBar(
       centerTitle: true,
@@ -115,6 +196,47 @@ class _HomePageState extends State<HomePage> {
                 colors: <Color>[Colors.purple, Colors.blue])),
       ),
       //backgroundColor: Color(0x9C4FF2),
+    );
+  }
+
+  Widget buildList(BuildContext context) {
+    return ListView.builder(
+      //itemCount: products.length,
+      itemBuilder: (context, index) {
+        return Column(
+          children: <Widget>[
+            Container(
+                constraints: BoxConstraints.expand(
+                  height: Theme.of(context).textTheme.display1.fontSize * 1.1 +
+                      200.0,
+                ),
+                color: Colors.white10,
+                alignment: Alignment.center,
+                child: Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      ListTile(
+                        title: Text(products[index].product_name),
+                        subtitle: (Text(products[index].brand_name + "\n" + products[index].subhead + products[index].description)),
+                      ),
+                      ButtonTheme(
+// make buttons use the appropriate styles for cards
+                        child: ButtonBar(
+                          children: <Widget>[
+                            FlatButton(
+                              child: const Text('See More'),
+                              onPressed: () {/* ... */},
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )),
+          ],
+        );
+      },
     );
   }
 }
